e93c277a6f695f10a4de0f7e4baf8359c1daa282
Tony Whyman <tony.whyman@mccallumwhyman.com>
2020-03-19 16:33:54 +0000

    auto gen include files from import_defs.sh

---
+++ import_defs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+FBSRC=$1/src
+
+h2pas -i -e -o include/dyn_consts.inc $FBSRC/include/dyn_consts.h 
+h2pas -i -e -o include/iberror.inc $FBSRC/include/gen/iberror.h 
+h2pas -i -e -o include/dyn_consts.inc $FBSRC/include/dyn_consts.h
+sed 's/(unsigned char) *\([0-9]\)/\1/
+s/(unsigned short) *\([0-9]\)/\1/' $FBSRC/include/firebird/impl/blr.h >blr_tmp.h
+h2pas -i -e -o include/blr.inc blr_tmp.h
+h2pas -i -e -o include/consts_pub.inc $FBSRC/include/firebird/impl/consts_pub.h
+h2pas -i -e -o include/inf_pub.inc $FBSRC/include/firebird/impl/inf_pub.h    
+
+
