56d915a0b743b17a6611c47632eca92e582c46bc
Tony Whyman <tony.whyman@mccallumwhyman.com>
2021-06-09 12:05:40 +0000

    Tagging Revision 1-2-1

--- IB.pas
+++ IB.pas
@@ -135,8 +135,8 @@ const
   {Interface version information}
   FBIntf_Major = 1;
   FBIntf_Minor = 2;
-  FBIntf_Release = 0;
-  FBIntf_Version = '1.2.0';
+  FBIntf_Release = 1;
+  FBIntf_Version = '1.2.1';
 
 const
   {DPB, TPB and SPB Parameter Block Name Prefixes}
