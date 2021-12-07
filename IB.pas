ae0f5f21047695e5487ca35a90692eacee990385
Tony Whyman <tony.whyman@mccallumwhyman.com>
2021-12-07 11:59:42 +0000

    Tagging Revision 1-4-0

--- IB.pas
+++ IB.pas
@@ -134,9 +134,9 @@ uses
 const
   {Interface version information}
   FBIntf_Major = 1;
-  FBIntf_Minor = 3;
-  FBIntf_Release = 2;
-  FBIntf_Version = '1.3.2';
+  FBIntf_Minor = 4;
+  FBIntf_Release = 0;
+  FBIntf_Version = '1.4.0';
 
 const
   {DPB, TPB and SPB Parameter Block Name Prefixes}
