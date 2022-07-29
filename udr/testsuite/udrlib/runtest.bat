95c95bd3ae5ad08bd12257e27ea2d47a476de6bc
Tony Whyman <tony.whyman@mccallumwhyman.com>
2022-07-29 22:17:24 +0000

    Delphi Fixes

--- udr/testsuite/udrlib/runtest.bat
+++ udr/testsuite/udrlib/runtest.bat
@@ -9,13 +9,14 @@ REM This script will typically need to be run as "administrator"
 
 set FBINTFROOT=..\..\..
 set FIREBIRD=
+
 FOR %%G in (4_0 3_0) do (
   if EXIST "%ProgramFiles%\Firebird\Firebird_%%G\firebird.exe" (
-    set FIREBIRD=%ProgramFiles%\Firebird\Firebird_%%G
+    set FIREBIRD="%ProgramFiles%\Firebird\Firebird_%%G"
     goto FBFOUND
   )
   if EXIST "%ProgramFiles(x86)%\Firebird\Firebird_%%G\firebird.exe" (
-    set FIREBIRD=%ProgramFiles^(x86^)%\Firebird\Firebird_%%G
+    set FIREBIRD="%ProgramFiles(x86)%\Firebird\Firebird_%%G"
     goto FBFOUND
  )
 )
@@ -24,6 +25,8 @@ echo Unable to locate Firebird
 goto :EOF
 
 :FBFOUND
+rem remove double quotes
+set FIREBIRD=%FIREBIRD:~1,-1%
 echo Firebird set to %FIREBIRD%
 
 
@@ -85,7 +88,7 @@ if not Exist fbudrtests.dll (
 net stop "Firebird Server - DefaultInstance"
 copy /y testtext.txt  "%ProgramData%"
 copy /y test.empty "%ProgramData%"
-copy /y fbudrtests.dll testtext.txt test.empty "%FIREBIRD%\plugins\udr"
+copy /y fbudrtests.dll "%FIREBIRD%\plugins\udr"
 IF ERRORLEVEL 1 (
 
   echo Copy to udr directory failed
